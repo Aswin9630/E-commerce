38c6ebb1a02e14c0ee8dd34d115701e79c79adcd
Aswin Krishna <aswinkrishna005@gmail.com>
2024-10-28 19:23:41 +0530

    Home Page UI completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {assets} from '../assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col justify-around sm:grid grid-cols-[2fr_1fr_1fr] gap-14 my-10 mt-14 text-sm'>
+            <div>
+                <img src={assets.logo} className='w-24 mb-5' alt="" />
+                <p className='w-full md:w-2/3 text-gray-500'>"Redefining fashion with quality pieces that fit your style, comfort, and budget."</p>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div>
+                <p className='font-medium text-xl mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+1-234-565-789</li>
+                    <li>contact@aktrendz.com</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <hr/>
+            <p className='py-5 text-sm text-center text-gray-400'>Copyright 2024@ aktrendz.com - All Right Reserved.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/NewsLetter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NewsLetter = () => {
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+    }
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off🎈</p>
+        <form onSubmit={handleSubmit} className='w-full sm:w-1/2 items-center flex gap-3 mx-auto my-6 pl-3 border'>
+            <input className='w-full sm:flex-1 outline-none text-gray-500' type="email" placeholder='Enter your email' required/>
+            <button  className='bg-black text-white font-medium text-xs py-4 px-10' type='submit'>Subscibe</button>
+        </form>
+    </div>
+  )
+}
+
+export default NewsLetter

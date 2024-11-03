5f003a585360c706aeda870d736eb60475eeb6fa
Aswin Krishna <aswinkrishna005@gmail.com>
2024-11-03 12:51:11 +0530

    About and contact page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Title from "../components/Title"
+import NewsLetter from "../components/NewsLetter"
+import { assets } from '../assets/frontend_assets/assets'
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={"ABOUT"} text2={"US"}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Welcome to our e-commerce clothing store, where fashion meets convenience! We bring you a curated collection of trendy, high-quality clothing for all styles and occasions. Our mission is to offer a seamless shopping experience, from browsing to delivery, with a focus on sustainable practices and customer satisfaction. With a range of designs that balance comfort, elegance, and affordability, we’re here to help you express yourself confidently. Join us on this journey to redefine style—whether you’re looking for everyday essentials or statement pieces, our store has something special for everyone!</p>
+          <p></p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Our mission is to redefine the online shopping experience by offering high-quality, stylish clothing that empowers individuals to express their unique identity. We are committed to sustainability, using eco-friendly materials and ethical practices in every step of our production. Through exceptional customer service and a carefully curated collection, we strive to make fashion accessible, enjoyable, and meaningful for everyone. We believe in creating a positive impact, not only through our products but also by supporting a community that values both style and substance.</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We are dedicated to providing high-quality products, with each piece carefully inspected to meet our standards for durability, comfort, and style.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>From easy navigation to fast, reliable shipping, we’re here to bring convenience right to your doorstep.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Our customers are at the heart of everything we do, and we’re committed to providing friendly, responsive support at every stage of your shopping experience.</p>
+        </div>
+      </div>
+
+      <NewsLetter/>
+    </div>
   )
 }
 
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Title from '../components/Title'
+import NewsLetter from "../components/NewsLetter"
+import { assets } from '../assets/frontend_assets/assets'
 
 const Contact = () => {
   return (
-    <div></div>
+    <div>
+      <div className='text-center text-2xl pt-10 bborder-t'>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='text-gray-600 font-semibold text-xl'>Our Store</p>
+          <p className='text-gray-500'>456 Trendy Lane <br />Sector 12, Dwarka <br />New Delhi, Delhi 110078 <br />India</p>
+          <p className='text-gray-500'>Phone: +91 98765 43210 <br />Email: support@aktrendz.com</p>
+          <p className='text-gray-600'>Careers at AKTRENDZ</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsLetter/>
+    </div>
   )
 }
 

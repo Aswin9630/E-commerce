77c04384dd53329c22a8c501c697dc27e02920ef
Aswin Krishna <aswinkrishna005@gmail.com>
2024-11-15 10:23:48 +0530

    orders page UI & features Completed

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,9 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Title from "../components/Title"
 import { ShopContext } from '../context/ShopContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import { BACKEND_URL } from '../utils/constants'
 
 const Orders = () => {
-  const { products , currency } = useContext(ShopContext)
+  const { token , currency } = useContext(ShopContext)
+  const [orderData, setOrderData] = useState([]);
+
+  useEffect(()=>{
+    loadOrderData()
+  },[token])
+
+  const loadOrderData = async () =>{
+        try {
+          if(!token){
+            return null
+          }else{
+            const response = await axios.post(BACKEND_URL+'/api/order/userorders',{},{headers:{Authorization:token}})
+
+            let allOrderItems = []
+
+            response.data.orders.map((order)=>{
+              order.items.map((item)=>{
+                item['status'] = order.status
+                item['payment'] = order.payment
+                item['paymentMethod'] = order.paymentMethod
+                item['date'] = order.date
+                allOrderItems.push(item)
+              })
+            })
+            setOrderData(allOrderItems.reverse());
+            
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error(error.message)
+          
+        }
+  }
+
   return (
     <div className='border-t pt-16'>
       <div className='text-2xl'>
@@ -12,27 +49,28 @@ const Orders = () => {
 
       <div>
           {
-            products.slice(1,4).map((item,index)=>(
+            orderData.map((item,index)=>(
               <div key={index} className='py-4 border-t border-b text-gray-700 flex felx-col md:flex-row md:items-center md:justify-between gap-4' >
                 <div className='flex items-start gap-6 text-sm'>
                 <img className='w-16 sm:w-20' src={item.image[0]} alt="" />
                 <div>
                   <p className='sm:text-base font-medium'>{item.name}</p>
                   <div className='flex items-center gap-3 mt-2 text-base text-gray-700'>
-                    <p className='text-lg'>{currency}{item.price}</p>
-                    <p>Quantity:1</p>
-                    <p>Size:M</p>
+                    <p>{currency}{item.price}</p>
+                    <p>Quantity:{item.quantity}</p>
+                    <p>Size:{item.size}</p>
                   </div>
-                  <p className='mt-2'>Date:<span className='text-gray-400'>3, October 2024</span></p>
+                  <p className='mt-2'>Date:<span className='text-gray-400'>{new Date(item.date).toLocaleString()}</span></p>
+                  <p className='mt-2'>Payment:<span className='text-gray-400 mx-1'>{item.paymentMethod}</span></p>
                 </div>
                 </div>
 
                   <div className='flex justify-between md:w-1/2'>
                     <div className='flex items-center gap-2'>
                       <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
-                      <p className='text-sm md:text-base'>Ready To Ship</p>
+                      <p className='text-sm md:text-base'>{item.status}</p>
                     </div>
-                    <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
+                    <button onClick={loadOrderData} className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
                   </div>
 
               </div>

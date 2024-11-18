4f9db51432d7ed6141dbe91debaba6371af267f4
Aswin Krishna <aswinkrishna005@gmail.com>
2024-11-18 18:48:38 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,24 @@ import adminRoutes from "./routes/adminRoute.js"
 import productRoutes from "./routes/productRoute.js"
 import cartRoutes from './routes/cartRoute.js' 
 import orderRoutes from './routes/orderRoute.js' 
-
 dotenv.config()
+
+const allowedOrigins = [
+    "https://aktrendz-admin.vercel.app",
+    "https://aktrendz-frontend.vercel.app"
+  ];
+
+  const corsOptions = {
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., mobile apps or Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allows cookies to be sent if needed
+  };
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -18,7 +34,7 @@ connectDB()
 connectCloudinary()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
  
 app.use('/api/user',userRoutes)
 app.use('/api/admin',adminRoutes)

cf2dfdf2e6077f22fe688fd727f83109609bae13
Aswin Krishna <aswinkrishna005@gmail.com>
2024-11-19 20:58:49 +0530

    .

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
                 <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>Home</li>
+                    <li>Home</li> 
                     <li>About</li>
                     <li>Delivery</li>
                     <li>Privacy Policy</li>
